a14f6b2df979b8e3256f137dd0283418775e20ac
bochkov-vi <bochkov2006@yandex.ru>
2020-04-19 08:22:17 +1200

    --

--- duty-planning/src/test/java/com/bochkov/duty/PlanningTest.java
+++ duty-planning/src/test/java/com/bochkov/duty/PlanningTest.java
@@ -8,7 +8,6 @@ import com.bochkov.duty.planning.PlanningSpringConfiguration;
 import com.bochkov.duty.planning.service.PlanningService;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
-import org.apache.commons.math3.stat.descriptive.AggregateSummaryStatistics;
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +16,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -58,15 +58,17 @@ public class PlanningTest {
                 .map(d -> new DutyAssigment().setDay(d).setDutyType(dutyTypeRepository.findById(3).get())).collect(Collectors.toList()));
         DutyPlan plan = planningService.solve(dutyPlan);
         System.out.println(plan);
-        Map<Person,Long> map =plan.getDuties().stream().collect(Collectors.groupingBy(DutyAssigment::getPerson,Collectors.counting()));
+        Map<Person, Long> map = plan.getDuties().stream().collect(Collectors.groupingBy(DutyAssigment::getPerson, Collectors.counting()));
         System.out.println(Joiner.on("\n").withKeyValueSeparator("->").join(map));
+        System.out.println("====================================");
+        System.out.println(plan.getDuties().stream().sorted(Comparator.comparing(DutyAssigment::getDay)).map(d -> String.format("%s %s", d.getDay(), d.getPerson())).collect(Collectors.joining("\n")));
         System.out.println(plan.getScore());
     }
 
     @Test
     public void testStatistic() {
 
-        SummaryStatistics ss = Stream.of(2, 3, 4, 5, 6,7,8,9,1).collect(SummaryStatistics::new, SummaryStatistics::addValue, (s1, s2) -> {
+        SummaryStatistics ss = Stream.of(2, 3, 4, 5, 6, 7, 8, 9, 1).collect(SummaryStatistics::new, SummaryStatistics::addValue, (s1, s2) -> {
         });
         System.out.println(ss);
     }
