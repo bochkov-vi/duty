b235a7c0b9c5119093a08f2f35632067de71e181
bochkov <bochkov-vi@yandex.ru>
2020-04-24 11:59:52 +1200

    ---

--- duty-web/src/main/java/com/bochkov/duty/wicket/component/PeriodCollectionInput.java
+++ duty-web/src/main/java/com/bochkov/duty/wicket/component/PeriodCollectionInput.java
@@ -3,10 +3,12 @@ package com.bochkov.duty.wicket.component;
 import com.bochkov.duty.jpa.entity.Period;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.extensions.markup.html.form.datetime.LocalTimeTextField;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.FormComponentPanel;
@@ -16,13 +18,15 @@ import org.apache.wicket.markup.repeater.RefreshingView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.util.ListModel;
+import org.apache.wicket.validation.IValidatable;
+import org.apache.wicket.validation.IValidationError;
+import org.apache.wicket.validation.IValidator;
+import org.apache.wicket.validation.ValidationError;
 
 import java.time.Duration;
 import java.time.LocalTime;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class PeriodCollectionInput extends FormComponentPanel<Collection<Period>> {
 
@@ -43,9 +47,11 @@ public class PeriodCollectionInput extends FormComponentPanel<Collection<Period>
     protected void onInitialize() {
         super.onInitialize();
         innerModel.setObject(Optional.ofNullable(getModel().getObject()).map(Lists::newArrayList).orElseGet(Lists::newArrayList));
-        this.setOutputMarkupId(true);
+        WebMarkupContainer container = new WebMarkupContainer("container");
+        add(container);
+        container.setOutputMarkupId(true);
         Form<Void> form = new Form<>("form");
-        add(form);
+        container.add(form);
         IModel<LocalTime> start = Model.of();
         IModel<Duration> duration = Model.of();
 
@@ -54,7 +60,7 @@ public class PeriodCollectionInput extends FormComponentPanel<Collection<Period>
         form.add(new AjaxSubmitLink("submit") {
             @Override
             protected void onSubmit(AjaxRequestTarget target) {
-                target.add(PeriodCollectionInput.this);
+                target.add(container);
                 innerModel.getObject().add(new Period(start.getObject(), duration.getObject()));
             }
         });
@@ -66,18 +72,31 @@ public class PeriodCollectionInput extends FormComponentPanel<Collection<Period>
 
             @Override
             protected void populateItem(Item<Period> item) {
-                item.add(new Label("start", item.getModel().map(Period::getStart)));
-                item.add(new Label("duration", item.getModel().map(Period::getDuration)));
+                item.add(new Label("start", item.getModel().map(Period::getStart).map(lt -> lt.format(DateTimeFormatter.ofPattern(getString("timePattern"))))));
+                item.add(new Label("duration", item.getModel().map(Period::getDuration).map(Duration::toMillis).map(m -> DurationFormatUtils.formatDuration(m, getString("timePattern")))));
                 item.add(new AjaxLink<Period>("delete", item.getModel()) {
                     @Override
                     public void onClick(AjaxRequestTarget target) {
-                        target.add(PeriodCollectionInput.this);
+                        target.add(container);
                         innerModel.getObject().remove(getModelObject());
                     }
                 });
             }
         };
-        add(listView);
+        container.add(listView);
+        add(new IValidator<Collection<Period>>() {
+            @Override
+            public void validate(IValidatable<Collection<Period>> validatable) {
+                if (validatable != null) {
+                    Set<Period> set = Sets.newTreeSet();
+                    boolean duplicates = validatable.getValue().stream().anyMatch(period -> !set.add(period));
+                    if (duplicates) {
+                        IValidationError error = new ValidationError("Периоды пересекаются");
+                        validatable.error(error);
+                    }
+                }
+            }
+        });
     }
 
     @Override
