01b211db56d027151b27fa004a43c4070cc667d3
bochkov <bochkov-vi@yandex.ru>
2020-06-15 15:10:06 +1200

    ---

--- duty-web/src/main/java/com/bochkov/duty/wicket/component/InputSpinner.java
+++ duty-web/src/main/java/com/bochkov/duty/wicket/component/InputSpinner.java
@@ -1,11 +1,16 @@
 package com.bochkov.duty.wicket.component;
 
+import com.google.common.collect.Lists;
 import de.agilecoders.wicket.webjars.request.resource.WebjarsJavaScriptResourceReference;
+import org.apache.wicket.Application;
+import org.apache.wicket.markup.head.HeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
+
+import java.util.List;
 
 public class InputSpinner extends TextField<Number> {
 
@@ -26,7 +31,12 @@ public class InputSpinner extends TextField<Number> {
     @Override
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
-        response.render(JavaScriptHeaderItem.forReference(new WebjarsJavaScriptResourceReference("bootstrap-input-spinner/current/src/bootstrap-input-spinner.js")));
+        response.render(JavaScriptHeaderItem.forReference(new WebjarsJavaScriptResourceReference("bootstrap-input-spinner/current/src/bootstrap-input-spinner.js") {
+            @Override
+            public List<HeaderItem> getDependencies() {
+                return Lists.newArrayList(JavaScriptHeaderItem.forReference(Application.get().getJavaScriptLibrarySettings().getJQueryReference()));
+            }
+        }));
         response.render(OnDomReadyHeaderItem.forScript(String.format("$('#%s').inputSpinner()", getMarkupId())));
     }
 }
--- duty-web/src/main/java/com/bochkov/duty/wicket/page/report/ShiftGridPage.java
+++ duty-web/src/main/java/com/bochkov/duty/wicket/page/report/ShiftGridPage.java
@@ -8,14 +8,22 @@ import com.bochkov.duty.jpa.repository.ShiftTypeRepository;
 import com.bochkov.duty.wicket.page.BootstrapPage;
 import com.bochkov.wicket.data.model.PersistableModel;
 import com.google.common.collect.Lists;
+import org.apache.wicket.extensions.markup.html.repeater.data.grid.ICellPopulator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.LambdaColumn;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.Fragment;
+import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
 
-@MountPath("report-shift-types")
+import java.time.LocalDate;
+
+@MountPath("shift-grid")
 public class ShiftGridPage extends BootstrapPage<Report> {
 
     @SpringBean
@@ -31,6 +39,25 @@ public class ShiftGridPage extends BootstrapPage<Report> {
         super();
         Integer pk = report.getId();
         setModel(LoadableDetachableModel.of(() -> reportRepository.findById(pk).get()));
+    }
+
+    public ShiftGridPage(PageParameters parameters) {
+        super(parameters);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        Integer pk = getPageParameters().get(0).toOptionalInteger();
+        if (pk == null && pk <= 0) {
+            pk = getPageParameters().get("id").toOptionalInteger();
+        }
+        if (pk != null && pk > 0) {
+            setModel(PersistableModel.of(pk, reportRepository::findById));
+        }
+
+
         ShiftGridPanel<ShiftType> grid = new ShiftGridPanel<ShiftType>("grid",
                 LoadableDetachableModel.of(shiftTypeRepository::findAll),
                 getModel().map(Report::getDateFrom).getObject(),
@@ -39,12 +66,19 @@ public class ShiftGridPage extends BootstrapPage<Report> {
             public IModel<ShiftType> model(ShiftType object) {
                 return PersistableModel.of(object, shiftTypeRepository::findById);
             }
+
+            @Override
+            public void populateItem(Item<ICellPopulator<ShiftType>> cellItem, String componentId, IModel<ShiftType> rowModel, LocalDate date) {
+                Fragment fragment = new Fragment(componentId, "cell-input", getPage());
+                fragment.add(new TextField<Integer>("input", Model.of()));
+                cellItem.add(fragment);
+            }
         };
 
         grid.setFirstColumns(Lists.newArrayList(new LambdaColumn<ShiftType, String>(
                 new ResourceModel("shiftType"),
                 ShiftType::toString
-        ){
+        ) {
             @Override
             public int getHeaderRowspan() {
                 return 2;
