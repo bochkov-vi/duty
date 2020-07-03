ff4d133e79e1e844b5b3ae368ecbd091499b4f46
bochkov <bochkov-vi@yandex.ru>
2020-07-03 23:05:58 +1200

    ---

--- duty-web/src/main/java/com/bochkov/duty/wicket/page/report/grid/GridPanel.java
+++ duty-web/src/main/java/com/bochkov/duty/wicket/page/report/grid/GridPanel.java
@@ -1,11 +1,18 @@
 package com.bochkov.duty.wicket.page.report.grid;
 
+import com.bochkov.duty.jpa.entity.Day;
 import com.bochkov.duty.jpa.entity.Employee;
+import com.bochkov.duty.jpa.repository.DayRepository;
 import com.bochkov.duty.jpa.repository.EmployeeRepository;
 import com.bochkov.wicket.data.model.nonser.CollectionModel;
 import com.google.common.collect.Lists;
+import org.apache.wicket.ClassAttributeModifier;
+import org.apache.wicket.Component;
+import org.apache.wicket.StyleAttributeModifier;
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
+import org.apache.wicket.markup.head.OnDomReadyHeaderItem;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
@@ -20,23 +27,28 @@ import javax.inject.Inject;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class GridPanel<T> extends GenericPanel<T> {
 
     @Inject
     EmployeeRepository employeeRepository;
+    @Inject
+    DayRepository dayRepository;
+
     WebMarkupContainer container = new WebMarkupContainer("container");
     IModel<LocalDate> modelStart;
+    IModel<LocalDate> modelEnd;
     IModel<Collection<Employee>> employeesModel;
     IModel<Integer> borderModel = Model.of(3);
+    String firstColumnDateId = null;
 
     public GridPanel(String id) {
         super(id);
         employeesModel = CollectionModel.of(employeeRepository::findById);
         employeesModel.setObject(employeeRepository.findAll());
         modelStart = Model.of(LocalDate.now().with(TemporalAdjusters.firstDayOfMonth()));
+        modelEnd = LoadableDetachableModel.of(() -> modelStart.getObject().with(TemporalAdjusters.lastDayOfMonth()));
     }
 
 
@@ -44,6 +56,7 @@ public class GridPanel<T> extends GenericPanel<T> {
         super(id);
         this.modelStart = Model.of(dateOfMonth.with(TemporalAdjusters.firstDayOfMonth()));
         this.employeesModel = employeesModel;
+        modelEnd = LoadableDetachableModel.of(() -> modelStart.getObject().with(TemporalAdjusters.lastDayOfMonth()));
     }
 
     public GridPanel(String id, IModel<T> model) {
@@ -56,10 +69,10 @@ public class GridPanel<T> extends GenericPanel<T> {
 
         setOutputMarkupId(true);
 
-        IModel<List<LocalDate>> columnModel = LoadableDetachableModel.of(() -> {
+        IModel<List<LocalDate>> columnsModel = LoadableDetachableModel.of(() -> {
             List<LocalDate> result = Lists.newArrayList();
-            LocalDate start = modelStart.combineWith(borderModel, (s, b) -> s.minusDays(b)).getObject();
-            LocalDate end = modelStart.combineWith(borderModel, (s, b) -> s.with(TemporalAdjusters.lastDayOfMonth()).plusDays(b)).getObject();
+            LocalDate start = modelStart.combineWith(borderModel, (date, b) -> date.minusDays(b)).getObject();
+            LocalDate end = modelEnd.combineWith(borderModel, (date, b) -> date.plusDays(b)).getObject();
 
             for (LocalDate date = start; !date.isAfter(end); date = date.plusDays(1)) {
                 result.add(date);
@@ -69,24 +82,81 @@ public class GridPanel<T> extends GenericPanel<T> {
         IModel<List<Employee>> rowModel = LoadableDetachableModel.of(() -> employeesModel.map(Lists::newArrayList).getObject());
         ListView<Employee> primaryColumn = new ListView<Employee>("primary-column", rowModel) {
             @Override
-            protected void populateItem(ListItem<Employee> item) {
-                item.add(new Label("cell", item.getModel().map(Employee::toString)));
+            protected void populateItem(ListItem<Employee> cellItem) {
+                cellItem.add(new Label("cell", cellItem.getModel().map(Employee::toString)));
+                cellItem.add(new AttributeAppender("data-employee-id", cellItem.getModel().map(Employee::getId)));
             }
         };
         add(primaryColumn);
 
 
-        ListView<LocalDate> columns = new ListView<LocalDate>("columns", columnModel) {
+        ListView<LocalDate> columns = new ListView<LocalDate>("columns", columnsModel) {
             @Override
             protected void populateItem(ListItem<LocalDate> columnItem) {
+
+                IModel<Day> dayModel = LoadableDetachableModel.of(() -> dayRepository.findOrCreate(columnItem.getModelObject()));
+
+                if (dayModel.isPresent().getObject()) {
+                    columnItem.add(new StyleAttributeModifier() {
+                        @Override
+                        protected Map<String, String> update(Map<String, String> oldStyles) {
+                            switch (dayModel.map(Day::getDaysToWeekend).getObject()) {
+                                case 0: {
+                                    oldStyles.put("background", "pink");
+                                    break;
+                                }
+                                case 1: {
+                                    oldStyles.put("background", "rgb(255,228,181)");
+                                    break;
+                                }
+                                case 2: {
+                                    oldStyles.put("background", "rgb(255,255,224)");
+                                    break;
+                                }
+                            }
+                            return oldStyles;
+                        }
+                    });
+                }
                 ListView<Employee> cells = new ListView<Employee>("cells", rowModel) {
                     @Override
                     protected void populateItem(ListItem<Employee> cellItem) {
-                        cellItem.add(new Label("cell", "-"));
+                        pupulateCellItem(cellItem, "cell", columnItem.getModel(), cellItem.getModel()).add(new AttributeAppender("data-employee-id", cellItem.getModel().map(Employee::getId)));
                     }
                 };
+
+                boolean enabled = columnItem.getModel().combineWith(modelStart, (columnDate, start) -> !columnDate.isBefore(start)).getObject()
+                        && columnItem.getModel().combineWith(modelEnd, (columnDate, end) -> !columnDate.isAfter(end)).getObject();
+                columnItem.setEnabled(enabled);
+                IModel<String> ddMmModel = columnItem.getModel().map(d -> d.format(DateTimeFormatter.ofPattern("dd.MM")));
+                if (!enabled) {
+                    columnItem.add(new AttributeAppender("disabled", true));
+                    columnItem.add(new ClassAttributeModifier() {
+                        @Override
+                        protected Set<String> update(Set<String> oldClasses) {
+                            oldClasses.add("disabled");
+                            return oldClasses;
+                        }
+                    });
+                    columnItem.add(new StyleAttributeModifier() {
+                        @Override
+                        protected Map<String, String> update(Map<String, String> oldStyles) {
+                            oldStyles.put("background", " rgb(119,136,153)");
+                            return oldStyles;
+                        }
+                    });
+                } else if (firstColumnDateId == null) {
+                    firstColumnDateId = ddMmModel.getObject();
+                }
+                if (dayModel.map(Day::getId).map(date -> Objects.equals(date, LocalDate.now())).getObject()) {
+                    firstColumnDateId = ddMmModel.getObject();
+                }
+
+
                 columnItem.add(cells);
-                columnItem.add(new Label("date-cell", columnItem.getModel().map(d -> d.format(DateTimeFormatter.ofPattern("dd.MM")))));
+
+                columnItem.add(new Label("date-cell", ddMmModel));
+                columnItem.add(new AttributeAppender("data-date-id", ddMmModel));
                 columnItem.add(new Label("day-of-week-cell", columnItem.getModel().map(d -> d.format(DateTimeFormatter.ofPattern("EE")))));
 
             }
@@ -97,10 +167,19 @@ public class GridPanel<T> extends GenericPanel<T> {
 
     }
 
+    public Component pupulateCellItem(ListItem<Employee> cellItem, String compId, IModel<LocalDate> dateModel, IModel<Employee> employeeModel) {
+        Component component = new Label(compId, "-");
+        cellItem.add(component);
+        return component;
+    }
+
     @Override
     public void renderHead(IHeaderResponse response) {
         super.renderHead(response);
         response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(GridPanel.class, "GridPanel.js")));
-
+        response.render(OnDomReadyHeaderItem.forScript("enableCellHover()"));
+        if (firstColumnDateId != null) {
+            response.render(OnDomReadyHeaderItem.forScript(String.format("scrollToDateColumn('%s','%s')", container.getMarkupId(), firstColumnDateId)));
+        }
     }
 }
--- duty-web/src/main/java/com/bochkov/duty/wicket/page/report/grid/GridPanel.js
+++ duty-web/src/main/java/com/bochkov/duty/wicket/page/report/grid/GridPanel.js
@@ -1,9 +1,39 @@
 function enableDetectScroll(element) {
     $(element).scroll(function () {
-        console.log( $(this).scrollLeft() + $(this).innerWidth() + ":" +$(this)[0].scrollWidth);
+        console.log($(this).scrollLeft() + $(this).innerWidth() + ":" + $(this)[0].scrollWidth);
         if ($(this).scrollLeft() + $(this).innerWidth() >= $(this)[0].scrollWidth) {
             console.log("scroll-end")
             $(this).trigger('scroll-end');
         }
     })
+}
+
+function enableCellHover() {
+    $("[data-employee-id]").mouseover(function () {
+        $("[data-employee-id='" + $(this).data("employee-id") + "']").addClass("border-primary border-bottom border-top border-right-0 border-left-0 bg-light text-dark disabled ");
+    });
+    $("[data-employee-id]").mouseleave(function () {
+        $("[data-employee-id='" + $(this).data("employee-id") + "']").removeClass("border-primary border-bottom border-top border-right-0 border-left-0 bg-light text-dark");
+    });
+
+
+    $("[data-date-id]").mouseover(function () {
+        $("[data-date-id='" + $(this).data("date-id") + "']").addClass("border-primary border-left border-right border-top-0 border-bottom-0 bg-light text-dark");
+    });
+    $("[data-date-id]").mouseleave(function () {
+        $("[data-date-id='" + $(this).data("date-id") + "']").removeClass("border-primary border-left border-right border-top-0 border-bottom-0 bg-light text-dark");
+    });
+};
+
+function scrollToDateColumn(containerId, dateId) {
+    var cont = $('#' + containerId);
+    var elem = $("[data-date-id='" + dateId + "']");
+    var contOffsetLeft = cont.offset().left;
+    var elemOffsetLeft = elem.offset().left;
+    var contScrollLeft = cont.scrollLeft();
+    var scrollLeft = elemOffsetLeft - contOffsetLeft + contScrollLeft;
+    cont.animate({
+        scrollLeft: scrollLeft
+    }, 500)
+
 }
