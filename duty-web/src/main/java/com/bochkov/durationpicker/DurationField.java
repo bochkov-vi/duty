2ddb1b1d88032f1eb83edb1254681379f85fd652
bochkov-vi <bochkov2006@yandex.ru>
2021-01-12 08:14:04 +1200

    --

--- duty-web/src/main/java/com/bochkov/durationpicker/DurationField.java
+++ duty-web/src/main/java/com/bochkov/durationpicker/DurationField.java
@@ -2,13 +2,11 @@ package com.bochkov.durationpicker;
 
 import com.bochkov.bootstrap.BootstrapBehavior;
 import com.google.common.collect.Lists;
-import com.google.common.primitives.Ints;
-import org.apache.commons.lang.time.DurationFormatUtils;
-import org.apache.wicket.ClassAttributeModifier;
-import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.markup.head.*;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.request.resource.PackageResourceReference;
 import org.apache.wicket.util.convert.ConversionException;
 import org.apache.wicket.util.convert.IConverter;
@@ -17,19 +15,18 @@ import java.time.Duration;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
-import java.util.Set;
 
 public class DurationField extends TextField<Duration> {
+
     IConverter<Duration> converter = new IConverter<Duration>() {
         @Override
         public Duration convertToObject(String value, Locale locale) throws ConversionException {
-            return Optional.ofNullable(value).map(str -> str.split(":")).map(a -> new Integer[]{Ints.tryParse(a[0]), Ints.tryParse(a[1])})
-                    .map(a -> Duration.ZERO.plusHours(a[0]).plusMinutes(a[1])).orElse(null);
+            return Optional.ofNullable(getConverter(Long.class)).map(cnv -> cnv.convertToObject(value, locale)).map(lng -> Duration.ofSeconds(lng)).orElse(null);
         }
 
         @Override
         public String convertToString(Duration value, Locale locale) {
-            return Optional.ofNullable(value).map(d -> DurationFormatUtils.formatDuration(d.toMillis(), "HH:mm")).orElse(null);
+            return Optional.ofNullable(value).map(dr -> dr.toMillis() / 1000).map(sec -> getConverter(Long.class).convertToString(sec, locale)).orElse(null);
         }
     };
 
@@ -44,14 +41,6 @@ public class DurationField extends TextField<Duration> {
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        add(new ClassAttributeModifier() {
-            @Override
-            protected Set<String> update(Set<String> oldClasses) {
-                oldClasses.add("html-duration-picker");
-                return oldClasses;
-            }
-        });
-        add(new AttributeAppender("data-duration", () -> getDefaultModelObjectAsString()));
     }
 
     @Override
@@ -71,13 +60,16 @@ public class DurationField extends TextField<Duration> {
 
     @Override
     public void renderHead(IHeaderResponse response) {
-        response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(DurationField.class, "DurationField.js") {
+        response.render(JavaScriptHeaderItem.forReference(new PackageResourceReference(DurationField.class, "bootstrap-duration-picker.js") {
             @Override
             public List<HeaderItem> getDependencies() {
                 return Lists.newArrayList(JavaScriptHeaderItem.forReference(BootstrapBehavior.JS),
                         CssHeaderItem.forReference(BootstrapBehavior.CSS));
             }
         }));
-        response.render(OnDomReadyHeaderItem.forScript(String.format("DurationPicker($('#%s'))", getMarkupId())));
+        response.render(CssHeaderItem.forReference(new PackageResourceReference(DurationField.class, "bootstrap-duration-picker.css")));
+        response.render(OnDomReadyHeaderItem.forScript(String.format("$('%s').durationPicker")));
+
+
     }
 }
