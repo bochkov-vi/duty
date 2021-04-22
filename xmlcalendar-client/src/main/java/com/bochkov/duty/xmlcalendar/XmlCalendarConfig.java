56aef277ef3da86fc2099cc50220b11a42d25d62
bochkov-vi <bochkov2006@yandex.ru>
2021-04-22 13:04:01 +1200

    --

---
+++ xmlcalendar-client/src/main/java/com/bochkov/duty/xmlcalendar/XmlCalendarConfig.java
@@ -0,0 +1,10 @@
+package com.bochkov.duty.xmlcalendar;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class XmlCalendarConfig {
+
+}
